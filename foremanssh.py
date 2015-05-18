3e5433b814ab8789f727acafbd9036aa2eac7962
Anthony Lapenna <lapenna.anthony@gmail.com>
2015-05-18 18:27:43 +0200

    Added authentication management. Added status code verification.

--- foremanssh.py
+++ foremanssh.py
@@ -4,27 +4,41 @@ import requests
 
 @click.command()
 @click.argument('foreman-api')
+@click.option('--user', help="User used to authenticate against Foreman.")
+@click.option('--password',
+              help="Password used to authenticate against Foreman.")
 @click.option('--search', help="Foreman search string")
-def main(foreman_api, search):
-    """ Usage: foreman-api http://foreman.wit.prod/api/hosts """
+def main(foreman_api, user, password, search):
+    """ Usage: foreman-api http://foreman.domain/api """
+    if user and password:
+        authentication = (user, password)
+    else:
+        authentication = None
+
     parameters = {'search': search}
     foreman_api_host_endpoint = ''.join([foreman_api, '/hosts'])
     r = requests.get(foreman_api_host_endpoint,
-                     auth=('user', 'password'),
+                     auth=authentication,
                      params=parameters)
     response_json = r.json()
-    print(foreman_api_host_endpoint)
-    for element in response_json:
-        host_id = element["host"]["id"]
-        process_host(foreman_api_host_endpoint, host_id)
+    if r.status_code == 200:
+        for element in response_json:
+            host_id = element["host"]["id"]
+            process_host(foreman_api_host_endpoint, authentication, host_id)
+            break
+    else:
+        print(response_json)
 
 
-def process_host(base_url, host_id):
+def process_host(base_url, authentication, host_id):
     print("HostID: %s" % host_id)
     foreman_host_api_url = ''.join([base_url,
                                     '/',
                                     str(host_id)])
-    r2 = requests.get(foreman_host_api_url,
-                      auth=('user', 'password'))
-    r2_json = r2.json()
-    print("IP: %s" % r2_json["host"]["ip"])
+    r = requests.get(foreman_host_api_url,
+                     auth=authentication)
+    response_json = r.json()
+    if r.status_code == 200:
+        print("IP: %s" % response_json["host"]["ip"])
+    else:
+        print(response_json)
