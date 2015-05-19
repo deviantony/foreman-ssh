2022b455c30386dcd8b4e1be8c1ba87cd37a5286
tony <alapenna@workit.fr>
2015-05-19 11:27:37 +0200

    Created foreman_ssh module.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='foreman-ssh',
     version='0.1',
-    py_modules=['foremanssh'],
+    py_modules=['foreman_ssh'],
     install_requires=[
         'click',
         'requests',
@@ -11,6 +11,6 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        foreman-ssh=foremanssh:main
+        foreman-ssh=foreman_ssh.foreman_ssh:main
     ''',
 )
